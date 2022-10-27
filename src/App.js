2c9ed6d043b022cd7794cb4d4a8bbe02b03a2cc1
esraaelgehany <esraa.abdallrhem40@gmail.com>
2022-10-28 00:50:35 +0200

    added 5 pages

--- src/App.js
+++ src/App.js
@@ -2,11 +2,15 @@ import React from 'react';
 import Alartjobs from './view/jobs/sideBar/jobAlerts'
 import AppSetting from './view/jobs/sideBar/applicationSetting';
 import ResumeBuilder from './view/jobs/sideBar/resumeBuilder';
+import Salary from './view/jobs/sideBar/salary';
+import JobSeekerfirst from './view/jobs/sideBar/jobseek/jobSeekerGudiance';
+import JobSeekersecond from './view/jobs/sideBar/jobseek/jobseeksecond';
+import Checklist from './view/jobs/sideBar/jobseek/chesclist';
 
 function App() {
   return (
     <>
-    <ResumeBuilder/>
+    <Checklist/>
     </>
   );
 }
