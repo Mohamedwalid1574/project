a0d12a5926a0bb8ea9673338930730ec628b91e8
Mohamedwalid1574 <mw774367@gmail.com>
2022-11-01 15:07:12 +0200

    add firebase file

---
+++ src/model/firebaseConfige.js
@@ -0,0 +1,14 @@
+
+import { initializeApp } from "firebase/app";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyBDqVEK9bOuEMxTpx1CEzoIT9sbu52z_OI",
+  authDomain: "final-project-44a7f.firebaseapp.com",
+  projectId: "final-project-44a7f",
+  storageBucket: "final-project-44a7f.appspot.com",
+  messagingSenderId: "762285782259",
+  appId: "1:762285782259:web:a9af10dbece943c222b386"
+};
+
+
+ export const app = initializeApp(firebaseConfig);
