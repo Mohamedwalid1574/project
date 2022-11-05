b189a4da38fe55fd330c69a3b24db89c3e60d45d
esraaelgehany <esraa.abdallrhem40@gmail.com>
2022-11-05 21:59:27 +0200

    sign in and up

---
+++ src/view/signup/in/signup.js
@@ -0,0 +1,61 @@
+import {Link} from "react-router-dom";
+import './signup.css';
+import LI from './Images/LI.png';
+import Black_logo from'./Images/black-logo.png';
+import G_logo  from './Images/g-logo.png';
+
+function SignUp() {
+  console.log("every thing is good")
+  return (
+    <>
+  <div className="row col-md-10 offset-md-1">
+  <div >
+  <div className="NGR-header"><img className='NGR-logo' src={LI}/></div>
+  <h1>Make the most of your professional life</h1>
+  <form className="NGR-frm  offset-md-4">
+    <label>Email</label><br></br>
+    <input type="email"></input><br></br>
+    <label>Password (6 or more characters)</label><br></br>
+    <input type="password"></input><br></br>
+    <p className="NGR-user-copyright">
+        By clicking Agree & Join, you agree to the LinkedIn 
+        <a className="NGR-user-copyrights">User Agreement</a>,
+        <a className="NGR-user-copyrights">Privacy Policy</a>, and 
+        <a className="NGR-user-copyrights">Cookie Policy</a>.
+    </p>
+    <button className="NGR-agree-btn " type="submit">Agree & Join</button><br></br>
+    <div className='d-flex'><hr></hr>    <span>&nbsp; &nbsp; &nbsp;or &nbsp; &nbsp; &nbsp;</span>   <hr></hr></div>
+    <button className="NGR-agree-btn-google" type="submit">Continue with Google<img className="NGR-g-logo" src={G_logo} /></button>
+    <p className="NGR-Already-on-LinkedIn">Already on LinkedIn <a className="NGR-user-copyrights">Sign in</a></p>
+  </form>
+  <p className="NGR-get-help ">Looking to create a page for a business?<a className="NGR-user-copyrights"> Get help </a></p>
+    </div>
+
+</div>
+    <div className='NGR-footer'>
+    <img className="NGR-black-logo "src={Black_logo}/>
+      <span className="NGR-footer-texts">©  2021</span>
+      <a className="NGR-footer-texts" href='www.google.com' >About</a>
+      <a className="NGR-footer-texts" href='www.google.com' >Accessibilty</a>
+      <a className="NGR-footer-texts" href='www.google.com' >User Agreement</a>
+      <a className="NGR-footer-texts" href='www.google.com' >Privacy Policy</a>
+      <a className="NGR-footer-texts" href='www.google.com' >Cookie Policy</a>
+      <a className="NGR-footer-texts" href='www.google.com' >Copyright Policy</a>
+      <a className="NGR-footer-texts" href='www.google.com' >Brand Policy</a>
+      <a className="NGR-footer-texts" href='www.google.com' >Guest Controls</a>
+      <a className="NGR-footer-texts" href='www.google.com' >Community Guidelines</a>
+      
+    <select className="NGR-slct">
+        <option>Language</option>
+        <option >English</option>
+        <option >Spanish</option>
+        <option  >Arabic</option>
+        <option >Germany</option>
+        </select>
+    
+    </div>
+    </>
+  );
+}
+ 
+export default SignUp;
